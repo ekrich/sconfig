a164f1b2841200c76bccd56592925bcca08cb45e
Eric K Richardson <ekrichardson@gmail.com>
2019-08-05 12:46:36 -0700

    Compile multiple versions of Scala Native (#40)
    * Prepare plugins for Scala Native and Scala.js cross publishing
    
    * Attempt to cross publish #1
    
    * Update cross version and format
    
    * Switch to minitest for Scala Native testing
    
    * Update Travis with extra Scala Native for test and release
    
    * Avoid jdk11 and SN 0.3.9 due to link error

--- sconfig/native/src/test/scala/ConfigFactoryTests.scala
+++ sconfig/native/src/test/scala/ConfigFactoryTests.scala
@@ -1,64 +1,62 @@
 package test
 
 import org.ekrich.config.ConfigFactory
-import utest._
+import minitest._
 import org.ekrich.config.ConfigRenderOptions
 
-object ConfigFactoryTests extends TestSuite {
-  println("Testing sconfig for Scala Native")
-  val tests = Tests {
-    test("load() check system property throws NotImplementedError") {
+object ConfigFactoryTests extends SimpleTestSuite {
 
-      // example of how system properties override; note this
-      // must be set before the config lib is used
-      val propKey = "simple-prop.whatever"
-      val propVal = "This value comes from a system property"
-      System.setProperty(propKey, propVal)
+  test("load() check system property throws NotImplementedError") {
 
-      // Load our own config values from the default location, application.conf
-      val e = intercept[NotImplementedError] {
-        val conf = ConfigFactory.load()
-        assert(conf.getString(propKey) == propVal)
-      }
+    // example of how system properties override; note this
+    // must be set before the config lib is used
+    val propKey = "simple-prop.whatever"
+    val propVal = "This value comes from a system property"
+    System.setProperty(propKey, propVal)
+
+    // Load our own config values from the default location, application.conf
+    val e = intercept[NotImplementedError] {
+      val conf = ConfigFactory.load()
+      assert(conf.getString(propKey) == propVal)
     }
+  }
 
-    test("parseString(s: String) scalafmt example works") {
-      val configStr =
-        """
-          |maxColumn = 100
-          |project.git=true
-          |align = none
-          |danglingParentheses = true
-          |newlines.neverBeforeJsNative = true
-          |newlines.sometimesBeforeColonInMethodReturnType = false
-          |assumeStandardLibraryStripMargin = true
+  test("parseString(s: String) scalafmt example works") {
+    val configStr =
+      """
+        |maxColumn = 100
+        |project.git=true
+        |align = none
+        |danglingParentheses = true
+        |newlines.neverBeforeJsNative = true
+        |newlines.sometimesBeforeColonInMethodReturnType = false
+        |assumeStandardLibraryStripMargin = true
         """.stripMargin
 
-      val conf = ConfigFactory.parseString(configStr)
+    val conf = ConfigFactory.parseString(configStr)
 
-      assert(conf.getInt("maxColumn") == 100)
-      assert(conf.getBoolean("project.git") == true)
-    }
+    assert(conf.getInt("maxColumn") == 100)
+    assert(conf.getBoolean("project.git") == true)
+  }
 
-    test("resolve() to force substitution") {
+  test("resolve() to force substitution") {
 
-      val configStr =
-        """
-          |pattern-default-main = default
-          |core = {
-          |  version: "0.1"
-          |  extends: [
-          |    ${pattern-default-main}
-          |  ]
-          |}
+    val configStr =
+      """
+        |pattern-default-main = default
+        |core = {
+        |  version: "0.1"
+        |  extends: [
+        |    ${pattern-default-main}
+        |  ]
+        |}
     """.stripMargin
 
-      val conf = ConfigFactory.parseString(configStr)
-      assert(conf.isResolved == false)
-      val rconf = conf.resolve()
-      assert(rconf.isResolved == true)
-      val pattern = rconf.getList("core.extends").get(0).unwrapped
-      assert(pattern == "default")
-    }
+    val conf = ConfigFactory.parseString(configStr)
+    assert(conf.isResolved == false)
+    val rconf = conf.resolve()
+    assert(rconf.isResolved == true)
+    val pattern = rconf.getList("core.extends").get(0).unwrapped
+    assert(pattern == "default")
   }
 }
