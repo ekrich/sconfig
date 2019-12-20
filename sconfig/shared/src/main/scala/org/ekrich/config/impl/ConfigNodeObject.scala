ddb6a9c66e6d4d9be7db38ad98d32f11b7c04141
Eric K Richardson <ekrichardson@gmail.com>
2019-12-19 16:24:17 -0800

    Fix non-local return in ConfigNodeObject part 2

--- sconfig/shared/src/main/scala/org/ekrich/config/impl/ConfigNodeObject.scala
+++ sconfig/shared/src/main/scala/org/ekrich/config/impl/ConfigNodeObject.scala
@@ -226,27 +226,28 @@ final class ConfigNodeObject private[impl] (
     ))
     // If the path is of length greater than one, see if the value needs to be added further down
     if (path.length > 1) {
-      var i = children.size - 1
-      while (i >= 0) {
-        breakable {
-          if (!children.get(i).isInstanceOf[ConfigNodeField])
-            break // continue
-          val node      = children.get(i).asInstanceOf[ConfigNodeField]
-          val key: Path = node.path.value
-          if (path.startsWith(key) && node.value
-                .isInstanceOf[ConfigNodeObject]) {
-            val remainingPath = desiredPath.subPath(key.length)
-            val newValue      = node.value.asInstanceOf[ConfigNodeObject]
-            childrenCopy.set(
-              i,
-              node.replaceValue(
-                newValue.addValueOnPath(remainingPath, value, flavor)
-              )
-            )
-            return new ConfigNodeObject(childrenCopy)
-          }
-        } // end break for continue in Java - increment needed as it was a for loop
-        i -= 1
+      val lastIndex = children.size - 1
+      val index = children.asScala.reverse.indexWhere { v =>
+        v match {
+          case node: ConfigNodeField =>
+            val key: Path = node.path.value
+            path.startsWith(key) && node.value.isInstanceOf[ConfigNodeObject]
+          case _ => false
+        }
+      }
+      if (index != -1) {
+        val i             = lastIndex - index
+        val node          = children.get(i).asInstanceOf[ConfigNodeField]
+        val key: Path     = node.path.value
+        val remainingPath = desiredPath.subPath(key.length)
+        val newValue      = node.value.asInstanceOf[ConfigNodeObject]
+        childrenCopy.set(
+          i,
+          node.replaceValue(
+            newValue.addValueOnPath(remainingPath, value, flavor)
+          )
+        )
+        return new ConfigNodeObject(childrenCopy)
       }
     }
     // Otherwise, construct the new setting
