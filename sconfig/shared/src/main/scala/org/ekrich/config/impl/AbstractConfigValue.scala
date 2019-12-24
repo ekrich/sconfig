b73dd2b589d1d346b1aea25ff977b4b305e7004e
Eric K Richardson <ekrichardson@gmail.com>
2019-12-24 08:32:58 -0800

    Update AbstractConfigValue to use case

--- sconfig/shared/src/main/scala/org/ekrich/config/impl/AbstractConfigValue.scala
+++ sconfig/shared/src/main/scala/org/ekrich/config/impl/AbstractConfigValue.scala
@@ -69,9 +69,11 @@ object AbstractConfigValue {
   ): Boolean =
     list.asScala.exists(_ == descendant) ||
       // now the expensive traversal
-      list.asScala.exists { v =>
-        v.isInstanceOf[Container] &&
-        v.asInstanceOf[Container].hasDescendant(descendant)
+      list.asScala.exists {
+        _ match {
+          case v: Container => v.hasDescendant(descendant)
+          case _            => false
+        }
       }
 
   def indent(
