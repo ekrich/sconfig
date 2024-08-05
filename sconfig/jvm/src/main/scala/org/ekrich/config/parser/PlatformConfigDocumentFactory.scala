cebc4a332e2a2ab70c8f5654fa71faca1f721c4b
Eric K Richardson <ekrichardson@gmail.com>
2024-08-05 10:13:43 -0700

    Initial refactor for ConfigDocumentFactory

---
+++ sconfig/jvm/src/main/scala/org/ekrich/config/parser/PlatformConfigDocumentFactory.scala
@@ -0,0 +1,6 @@
+package org.ekrich.config.parser
+
+/**
+ * [[ConfigFactoryDocument]] methods for Scala JVM platform
+ */
+abstract class PlatformConfigDocumentFactory extends ConfigDocumentFactoryJvmNative {}
