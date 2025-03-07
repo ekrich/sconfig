24a41ece973788776af5b4038d71f71b926af0a8
Scala Steward <scala_steward@virtuslab.com>
2025-03-07 17:36:18 +0000

    Reformat with scalafmt 3.9.3
    Executed command: scalafmt --non-interactive

--- sconfig/shared/src/main/scala/org/ekrich/config/impl/Tokenizer.scala
+++ sconfig/shared/src/main/scala/org/ekrich/config/impl/Tokenizer.scala
@@ -521,24 +521,24 @@ object Tokenizer {
       var t: Token = null
       breakable {
         while ({
-          t = pullNextToken(saver)
-          // note that we avoid validating the allowed tokens inside
-          // the substitution here; we even allow nested substitutions
-          // in the tokenizer. The parser sorts it out.
-          if (t eq Tokens.CLOSE_CURLY) { // end the loop, done!
-            break() // break
-          } else if (t eq Tokens.END) {
-            throw TokenIterator.problem(
-              origin,
-              "Substitution ${ was not closed with a }"
-            )
-          } else {
-            val whitespace = saver.check(t, origin, lineNumber)
-            if (whitespace != null) expression.add(whitespace)
-            expression.add(t)
-          }
-          true
-        }) ()
+              t = pullNextToken(saver)
+              // note that we avoid validating the allowed tokens inside
+              // the substitution here; we even allow nested substitutions
+              // in the tokenizer. The parser sorts it out.
+              if (t eq Tokens.CLOSE_CURLY) { // end the loop, done!
+                break() // break
+              } else if (t eq Tokens.END) {
+                throw TokenIterator.problem(
+                  origin,
+                  "Substitution ${ was not closed with a }"
+                )
+              } else {
+                val whitespace = saver.check(t, origin, lineNumber)
+                if (whitespace != null) expression.add(whitespace)
+                expression.add(t)
+              }
+              true
+            }) ()
       }
       Tokens.newSubstitution(origin, optional, expression)
     }
