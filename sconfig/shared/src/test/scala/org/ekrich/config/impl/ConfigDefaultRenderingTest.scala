97b6045b5057e11433a2aa7bcccc9c4642f3a591
Waldemar Wosinski <kastoestoramadus@gmail.com>
2025-11-13 20:56:36 +0100

    bug fix of breaking Arrays concatenation

---
+++ sconfig/shared/src/test/scala/org/ekrich/config/impl/ConfigDefaultRenderingTest.scala
@@ -0,0 +1,112 @@
+package org.ekrich.config.impl
+
+import org.ekrich.config.{
+  ConfigFactory,
+  ConfigFormatOptions,
+  ConfigParseOptions,
+  ConfigRenderOptions
+}
+import org.junit.*
+
+// Regression tests for rendering old behaviour compatibility
+class ConfigDefaultRenderingTest extends TestUtilsShared {
+  val parseOptions = ConfigParseOptions.defaults.setAllowMissing(true)
+  val myDefaultRenderOptions = ConfigRenderOptions.defaults
+    .setJson(false)
+    .setOriginComments(false)
+    .setComments(true)
+    .setFormatted(true)
+  implicit val defaultFormatOptions: ConfigFormatOptions =
+    ConfigFormatOptions.defaults
+
+  def formatHocon(
+      str: String
+  )(implicit configFormatOptions: ConfigFormatOptions): String =
+    ConfigFactory
+      .parseString(str, parseOptions)
+      .root
+      .render(
+        myDefaultRenderOptions.setConfigFormatOptions(configFormatOptions)
+      )
+
+  @Test
+  def newLineAtTheEnd(): Unit = {
+    val in = """r {
+               |}""".stripMargin
+    val result = formatHocon(in)
+    val expected = """r {}
+                     |""".stripMargin
+    checkEqualObjects(expected, result)
+  }
+
+  @Test
+  def useFourSpacesIndentation(): Unit = {
+    val in = """r {
+               |  p {
+               |        d {
+               |        s: ${r.ss}
+               |        }
+               |     }
+               |}""".stripMargin
+    val result = formatHocon(in)
+
+    val expected = """r {
+                     |    p {
+                     |        d {
+                     |            s = ${r.ss}
+                     |        }
+                     |    }
+                     |}
+                     |""".stripMargin
+    checkEqualObjects(expected, result)
+  }
+
+  @Test
+  def useEqualsAsAssignSign(): Unit = {
+    val in = """r {
+               |    s=t_f
+               |      "n-m"=1
+               |    n:"ALA"
+               |}""".stripMargin
+    val result = formatHocon(in)
+
+    val expected = """r {
+                     |    n = ALA
+                     |    n-m = 1
+                     |    s = t_f
+                     |}
+                     |""".stripMargin
+    checkEqualObjects(expected, result)
+  }
+
+  @Test
+  def dontSimplifyOneEntryNestedObjects(): Unit = {
+    val in = """r.p.d= 42"""
+    val result = formatHocon(in)
+
+    val expected =
+      """r {
+        |    p {
+        |        d = 42
+        |    }
+        |}
+        |""".stripMargin
+    checkEqualObjects(expected, result)
+  }
+
+  @Test
+  def properArrayConcat(): Unit = {
+    val in =
+      """except: ${ex1} ${ex2}
+        |myEmpty: " "
+        |""".stripMargin
+    val result = formatHocon(in)
+
+    val expected =
+      """except = ${ex1} ${ex2}
+        |myEmpty = " "
+        |""".stripMargin
+    checkEqualObjects(expected, result)
+  }
+
+}
