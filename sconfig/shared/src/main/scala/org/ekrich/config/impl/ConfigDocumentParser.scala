0cc271cbbaf0f8c39e51f7e59acd654ef8cf19fc
Eric K Richardson <ekrichardson@gmail.com>
2026-06-16 11:41:23 -0700

    Remove breaks from ConfigDocumentParser

--- sconfig/shared/src/main/scala/org/ekrich/config/impl/ConfigDocumentParser.scala
+++ sconfig/shared/src/main/scala/org/ekrich/config/impl/ConfigDocumentParser.scala
@@ -5,7 +5,7 @@ package org.ekrich.config.impl
 
 import java.lang as jl
 import java.util as ju
-import scala.util.control.Breaks._
+
 import org.ekrich.config._
 
 object ConfigDocumentParser {
@@ -78,25 +78,22 @@ object ConfigDocumentParser {
         nodes: ju.Collection[AbstractConfigNode]
     ): Token = {
       var retToken: Token = null // added for Scala
-
-      breakable {
-        while (true) {
-          val t: Token = nextToken
-          if (Tokens.isIgnoredWhitespace(t) || Tokens.isNewline(t) ||
-              ParseContext.isUnquotedWhitespace(t)) {
-            nodes.add(new ConfigNodeSingleToken(t))
-            if (Tokens.isNewline(t)) {
-              lineNumber = t.lineNumber + 1
-            }
-          } else if (Tokens.isComment(t)) {
-            nodes.add(new ConfigNodeComment(t))
-          } else {
-            val newNumber = t.lineNumber
-            if (newNumber >= 0) lineNumber = newNumber
-            // return t
-            retToken = t
-            break() // break - added for Scala to "return"
+      var continue = true
+      while (continue) {
+        val t: Token = nextToken
+        if (Tokens.isIgnoredWhitespace(t) || Tokens.isNewline(t) ||
+            ParseContext.isUnquotedWhitespace(t)) {
+          nodes.add(new ConfigNodeSingleToken(t))
+          if (Tokens.isNewline(t)) {
+            lineNumber = t.lineNumber + 1
           }
+        } else if (Tokens.isComment(t)) {
+          nodes.add(new ConfigNodeComment(t))
+        } else {
+          val newNumber = t.lineNumber
+          if (newNumber >= 0) lineNumber = newNumber
+          retToken = t
+          continue = false // break
         }
       }
       retToken
@@ -124,32 +121,30 @@ object ConfigDocumentParser {
       } else {
         var sawSeparatorOrNewline = false
         var t = nextToken
-        var retTrue = false // added for Scala for break below
-        breakable {
-          while (true) {
-            if (Tokens.isIgnoredWhitespace(t) || ParseContext
-                  .isUnquotedWhitespace(t))
-              nodes.add(new ConfigNodeSingleToken(t))
-            else if (Tokens.isComment(t)) nodes.add(new ConfigNodeComment(t))
-            else if (Tokens.isNewline(t)) {
-              sawSeparatorOrNewline = true
-              lineNumber += 1
-              nodes.add(new ConfigNodeSingleToken(t))
-              // we want to continue to also eat
-              // a comma if there is one.
-            } else if (t eq Tokens.COMMA) {
-              nodes.add(new ConfigNodeSingleToken(t))
-              // return true
-              retTrue = true
-              break() // break - added for Scala to "return"
-            } else {
-              // non-newline-or-comma
-              putBack(t)
-              // return sawSeparatorOrNewline
-              break() // break - added for Scala to "return"
-            }
-            t = nextToken
+        var retTrue = false // one case below we return true
+        var continue = true
+        while (continue) {
+          if (Tokens.isIgnoredWhitespace(t) || ParseContext
+                .isUnquotedWhitespace(t))
+            nodes.add(new ConfigNodeSingleToken(t))
+          else if (Tokens.isComment(t)) nodes.add(new ConfigNodeComment(t))
+          else if (Tokens.isNewline(t)) {
+            sawSeparatorOrNewline = true
+            lineNumber += 1
+            nodes.add(new ConfigNodeSingleToken(t))
+            // we want to continue to also eat
+            // a comma if there is one.
+          } else if (t eq Tokens.COMMA) {
+            nodes.add(new ConfigNodeSingleToken(t))
+            retTrue = true
+            continue = false // break
+          } else {
+            // non-newline-or-comma
+            putBack(t)
+            continue = false // break
           }
+          if (continue)
+            t = nextToken
         }
         if (retTrue) true else sawSeparatorOrNewline
       }
@@ -166,25 +161,24 @@ object ConfigDocumentParser {
       var valueCount = 0
       // ignore a newline up front
       var t = nextTokenCollectingWhitespace(nodes)
-
-      breakable {
-        while (true) {
-          var v: AbstractConfigNodeValue = null
-          if (Tokens.isIgnoredWhitespace(t)) {
-            values.add(new ConfigNodeSingleToken(t))
-          } else if (Tokens.isValue(t) || Tokens.isUnquotedText(t) ||
-              Tokens.isSubstitution(t) ||
-              (t eq Tokens.OPEN_CURLY) || (t eq Tokens.OPEN_SQUARE)) {
-            // there may be newlines _within_ the objects and arrays
-            v = parseValue(t)
-            valueCount += 1
-            if (v == null) throw new ConfigException.BugOrBroken("no value")
-            values.add(v)
-          } else {
-            break() // break
-          }
-          t = nextToken // but don't consolidate across a newline
+      var continue = true
+      while (continue) {
+        var v: AbstractConfigNodeValue = null
+        if (Tokens.isIgnoredWhitespace(t)) {
+          values.add(new ConfigNodeSingleToken(t))
+        } else if (Tokens.isValue(t) || Tokens.isUnquotedText(t) ||
+            Tokens.isSubstitution(t) ||
+            (t eq Tokens.OPEN_CURLY) || (t eq Tokens.OPEN_SQUARE)) {
+          // there may be newlines _within_ the objects and arrays
+          v = parseValue(t)
+          valueCount += 1
+          if (v == null) throw new ConfigException.BugOrBroken("no value")
+          values.add(v)
+        } else {
+          continue = false // break
         }
+        if (continue)
+          t = nextToken // but don't consolidate across a newline
       }
 
       putBack(t)
@@ -203,16 +197,16 @@ object ConfigDocumentParser {
       // Put back any trailing whitespace, as the parent object is responsible for tracking
       // any leading/trailing whitespace
       var i = values.size - 1
-      breakable {
-        while (i >= 0) {
-          if (values.get(i).isInstanceOf[ConfigNodeSingleToken]) {
-            putBack(values.get(i).asInstanceOf[ConfigNodeSingleToken].token)
-            values.remove(i)
-          } else {
-            break() // break
-          }
-          i -= 1
+      continue = true // reuse
+      while (i >= 0 && continue) {
+        if (values.get(i).isInstanceOf[ConfigNodeSingleToken]) {
+          putBack(values.get(i).asInstanceOf[ConfigNodeSingleToken].token)
+          values.remove(i)
+        } else {
+          continue = false // break
         }
+        if (continue)
+          i -= 1
       }
       new ConfigNodeConcatenation(values)
     }
@@ -393,93 +387,95 @@ object ConfigDocumentParser {
       val keys = new ju.HashMap[String, jl.Boolean]
       if (hadOpenCurly)
         objectNodes.add(new ConfigNodeSingleToken(Tokens.OPEN_CURLY))
-      breakable {
-        while (true) {
-          var t = nextTokenCollectingWhitespace(objectNodes)
-          if (t eq Tokens.CLOSE_CURLY) {
-            if ((flavor eq ConfigSyntax.JSON) && afterComma)
-              throw parseError(
-                addQuoteSuggestion(
-                  t.toString,
-                  "expecting a field name after a comma, got a close brace } instead"
-                )
+      var continue = true
+      while (continue) {
+        var t = nextTokenCollectingWhitespace(objectNodes)
+        if (t eq Tokens.CLOSE_CURLY) {
+          if ((flavor eq ConfigSyntax.JSON) && afterComma)
+            throw parseError(
+              addQuoteSuggestion(
+                t.toString,
+                "expecting a field name after a comma, got a close brace } instead"
               )
-            else if (!hadOpenCurly)
-              throw parseError(
-                addQuoteSuggestion(
-                  t.toString,
-                  "unbalanced close brace '}' with no open brace"
-                )
+            )
+          else if (!hadOpenCurly)
+            throw parseError(
+              addQuoteSuggestion(
+                t.toString,
+                "unbalanced close brace '}' with no open brace"
               )
-            objectNodes.add(new ConfigNodeSingleToken(Tokens.CLOSE_CURLY))
-            break() // break
-          } else if ((t eq Tokens.END) && !hadOpenCurly) {
-            putBack(t)
-            break() // break
-          } else if ((flavor ne ConfigSyntax.JSON) && ParseContext
-                .isIncludeKeyword(t)) {
-            val includeNodes =
-              new ju.ArrayList[AbstractConfigNode]
-            includeNodes.add(new ConfigNodeSingleToken(t))
-            objectNodes.add(parseInclude(includeNodes))
-            afterComma = false
+            )
+          objectNodes.add(new ConfigNodeSingleToken(Tokens.CLOSE_CURLY))
+          continue = false // break
+        } else if ((t eq Tokens.END) && !hadOpenCurly) {
+          putBack(t)
+          continue = false // break
+        } else if ((flavor ne ConfigSyntax.JSON) && ParseContext
+              .isIncludeKeyword(t)) {
+          val includeNodes =
+            new ju.ArrayList[AbstractConfigNode]
+          includeNodes.add(new ConfigNodeSingleToken(t))
+          objectNodes.add(parseInclude(includeNodes))
+          afterComma = false
+        } else {
+          keyValueNodes = new ju.ArrayList[AbstractConfigNode]
+          val keyToken = t
+          val path = parseKey(keyToken)
+          keyValueNodes.add(path)
+          val afterKey = nextTokenCollectingWhitespace(keyValueNodes)
+          var insideEquals = false
+          var nextValue: AbstractConfigNodeValue = null
+          if ((flavor eq ConfigSyntax.CONF) && (afterKey eq Tokens.OPEN_CURLY)) {
+            // can omit the ':' or '=' before an object value
+            nextValue = parseValue(afterKey)
           } else {
-            keyValueNodes = new ju.ArrayList[AbstractConfigNode]
-            val keyToken = t
-            val path = parseKey(keyToken)
-            keyValueNodes.add(path)
-            val afterKey = nextTokenCollectingWhitespace(keyValueNodes)
-            var insideEquals = false
-            var nextValue: AbstractConfigNodeValue = null
-            if ((flavor eq ConfigSyntax.CONF) && (afterKey eq Tokens.OPEN_CURLY)) {
-              // can omit the ':' or '=' before an object value
-              nextValue = parseValue(afterKey)
-            } else {
-              if (!isKeyValueSeparatorToken(afterKey))
-                throw parseError(
-                  addQuoteSuggestion(
-                    afterKey.toString,
-                    "Key '" + path.render + "' may not be followed by token: " + afterKey
-                  )
+            if (!isKeyValueSeparatorToken(afterKey))
+              throw parseError(
+                addQuoteSuggestion(
+                  afterKey.toString,
+                  "Key '" + path.render + "' may not be followed by token: " + afterKey
                 )
-              keyValueNodes.add(new ConfigNodeSingleToken(afterKey))
-              if (afterKey eq Tokens.EQUALS) {
-                insideEquals = true
-                equalsCount += 1
-              }
-              nextValue = consolidateValues(keyValueNodes)
-              if (nextValue == null)
-                nextValue =
-                  parseValue(nextTokenCollectingWhitespace(keyValueNodes))
+              )
+            keyValueNodes.add(new ConfigNodeSingleToken(afterKey))
+            if (afterKey eq Tokens.EQUALS) {
+              insideEquals = true
+              equalsCount += 1
             }
-            keyValueNodes.add(nextValue)
-            if (insideEquals) equalsCount -= 1
-            lastInsideEquals = insideEquals
-            val key = path.value.first
-            val remaining = path.value.remainder
-            if (remaining == null) {
-              val existing = keys.get(key)
-              if (existing != null) {
-                // In strict JSON, dups should be an error; while in
-                // our custom config language, they should be merged
-                // if the value is an object (or substitution that
-                // could become an object).
-                if (flavor eq ConfigSyntax.JSON)
-                  throw parseError(
-                    "JSON does not allow duplicate fields: '" + key + "' was already seen"
-                  )
-              }
-              keys.put(key, true)
-            } else {
-              if (flavor eq ConfigSyntax.JSON)
-                throw new ConfigException.BugOrBroken(
-                  "somehow got multi-element path in JSON mode"
-                )
-              keys.put(key, true)
-            }
-            afterComma = false
-            objectNodes.add(new ConfigNodeField(keyValueNodes))
+            nextValue = consolidateValues(keyValueNodes)
+            if (nextValue == null)
+              nextValue = parseValue(
+                nextTokenCollectingWhitespace(keyValueNodes)
+              )
           }
+          keyValueNodes.add(nextValue)
+          if (insideEquals) equalsCount -= 1
+          lastInsideEquals = insideEquals
+          val key = path.value.first
+          val remaining = path.value.remainder
+          if (remaining == null) {
+            val existing = keys.get(key)
+            if (existing != null) {
+              // In strict JSON, dups should be an error; while in
+              // our custom config language, they should be merged
+              // if the value is an object (or substitution that
+              // could become an object).
+              if (flavor eq ConfigSyntax.JSON)
+                throw parseError(
+                  "JSON does not allow duplicate fields: '" + key + "' was already seen"
+                )
+            }
+            keys.put(key, true)
+          } else {
+            if (flavor eq ConfigSyntax.JSON)
+              throw new ConfigException.BugOrBroken(
+                "somehow got multi-element path in JSON mode"
+              )
+            keys.put(key, true)
+          }
+          afterComma = false
+          objectNodes.add(new ConfigNodeField(keyValueNodes))
+        }
+        if (continue) { // second break chain
           if (checkElementSeparator(objectNodes)) {
             // continue looping
             afterComma = true
@@ -496,7 +492,7 @@ object ConfigDocumentParser {
                   )
                 )
               objectNodes.add(new ConfigNodeSingleToken(t))
-              break() // break
+              continue = false // break
             } else if (hadOpenCurly) {
               throw parseError(
                 addQuoteSuggestion(
@@ -509,7 +505,7 @@ object ConfigDocumentParser {
             } else {
               if (t eq Tokens.END) {
                 putBack(t)
-                break() // break
+                continue = false // break
               } else
                 throw parseError(
                   addQuoteSuggestion(
@@ -551,24 +547,23 @@ object ConfigDocumentParser {
           )
       }
       // now remaining elements
-      breakable {
-        while (true) {
-          // just after a value
-          if (checkElementSeparator(children)) {
-            // comma (or newline equivalent) consumed
-          } else {
-            t = nextTokenCollectingWhitespace(children)
-            if (t eq Tokens.CLOSE_SQUARE) {
-              children.add(new ConfigNodeSingleToken(t))
-              // return new ConfigNodeArray(children)
-              break() // break - added for Scala to force return from while loop
-            } else
-              throw parseError(
-                "List should have ended with ] or had a comma, instead had token: " +
-                  t + " (if you want " + t + " to be part of a string value, then double-quote it)"
-              )
-          }
-          // now just after a comma
+      var continue = true
+      while (continue) {
+        // just after a value
+        if (checkElementSeparator(children)) {
+          // comma (or newline equivalent) consumed
+        } else {
+          t = nextTokenCollectingWhitespace(children)
+          if (t eq Tokens.CLOSE_SQUARE) {
+            children.add(new ConfigNodeSingleToken(t))
+            continue = false // break
+          } else
+            throw parseError(
+              "List should have ended with ] or had a comma, instead had token: " +
+                t + " (if you want " + t + " to be part of a string value, then double-quote it)"
+            )
+        }
+        if (continue) { // now just after a comma
           nextValue = consolidateValues(children)
           if (nextValue != null) children.add(nextValue)
           else {
